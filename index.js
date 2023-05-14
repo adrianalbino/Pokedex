c01ea2e131ce232ed3337393da68113a2c820c7e
Adrian Albino <21102558@usc.edu.ph>
2023-05-14 23:01:49 +0800

    [Feature] Added popup

--- index.js
+++ index.js
@@ -30,8 +30,13 @@ async function getPokemonData(api)
 async function getPokemonDetails(api)
 {  
     let type2
+    let arrayTypes = []
+    let numberOfTypes
     const response = await fetch(api)
     const data = await response.json()
+    for (numberOfTypes = 0; numberOfTypes < data.types.length; numberOfTypes++){
+        arrayTypes = arrayTypes.concat(data.types[numberOfTypes].type.name)
+    }
     if(data.types[1]){
         type2 = capitalizeFirstLetter(data.types[1].type.name)
     } else {
@@ -43,31 +48,85 @@ async function getPokemonDetails(api)
     const details = {
         "name" : name,
         "types" : types,
+        "arrTypes": arrayTypes,
         "height" : height,
         "id" : data.id
     }
     return details
 }
 /* Display and render the pokemon */
-async function displayPokemon()
-{
-    await getPokemonData(url)
-    for (let x = 0; x < 10; x++) // limit to 10 
-    {
-        const details = await getPokemonDetails(`https://pokeapi.co/api/v2/pokemon/${count + 1}/`)
-        count++
-        pokemonList.innerHTML += `
-        <div class="pokemon-cards pokemon-card">
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${count}.png" <br> 
-            <p class="id">ID: ${details.id}</p>
-            <p class="name"> Name: ${details.name}</p>
-            <p class="type"> Types: ${details.types} </p>
-        </div>`
+async function displayPokemon() {
+    await getPokemonData(url);
+    for (let x = 0; x < 10; x++) {
+      const details = await getPokemonDetails(
+        `https://pokeapi.co/api/v2/pokemon/${count + 1}/`
+      );
+      count++;
+      const card = document.createElement("div");
+      card.classList.add("pokemon-cards", "pokemon-card");
+  
+      const image = document.createElement("img");
+      image.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${count}.png`;
+      card.addEventListener("click", () => {
+        openPopup(details);
+      });
+  
+      const id = document.createElement("p");
+      id.classList.add("id");
+      id.textContent = `ID: ${details.id}`;
+  
+      const name = document.createElement("p");
+      name.classList.add("name");
+      name.textContent = `Name: ${details.name}`;
+  
+      const type = document.createElement("p");
+      type.classList.add("type");
+      type.textContent = `Types: ${details.types}`;
+  
+      card.appendChild(image);
+      card.appendChild(id);
+      card.appendChild(name);
+      card.appendChild(type);
+  
+      pokemonList.appendChild(card);
     }
-}
-
-// Height: ${details.height} <br>
-/* Load button */
+  }
+  
+  async function openPopup(details) {
+    /* Remove existing popups */
+    const existingPopups = document.querySelectorAll(".popup");
+    existingPopups.forEach((popup) => popup.remove());
+    /* Popup creation */
+    const listofWeaknesses = await getWeakness(details.arrTypes)
+    console.log(listofWeaknesses)
+    const popup = document.createElement("div")
+    popup.classList.add("popup")
+    const image = document.createElement("img")
+    image.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${details.id}.png`
+    const id = document.createElement("p")
+    id.textContent = `ID: ${details.id}`
+    const name = document.createElement("p")
+    name.textContent = `Name: ${details.name}`
+    const type = document.createElement("p")
+    type.textContent = `Types: ${details.types}`
+    const height = document.createElement("p")
+    height.textContent = `Height: ${details.height}`
+    const weakness = document.createElement("p")
+    weakness.textContent = `Weaknesses: ${listofWeaknesses}`
+    /* Adding to div */
+    popup.appendChild(image)
+    popup.appendChild(id)
+    popup.appendChild(name)
+    popup.appendChild(type)
+    popup.appendChild(height)
+    popup.appendChild(weakness)
+  
+    document.body.appendChild(popup);
+    popup.addEventListener("click", () => { // closes popup when clicking on it
+        document.body.removeChild(popup);
+      });
+  }
+  
 loadButton.addEventListener("click", function()
 {
     displayPokemon()
@@ -75,7 +134,6 @@ loadButton.addEventListener("click", function()
 
 
 displayPokemon()
-startPokemons = pokemons
 
 
 
@@ -166,3 +224,74 @@ searchInput.addEventListener("input", async function()
     }
 
 })
+
+/* Get weakness */
+async function getWeakness(types)
+{
+    let weaknesses = []
+    let weakString = ""
+    for (let ctr = 0; ctr < types.length; ctr++) 
+    {
+        switch (types[ctr])
+        {
+            case "normal":
+                weaknesses = weaknesses.concat(["fighting"])
+                break
+            case "fighting":
+                weaknesses = weaknesses.concat(["flying", "psychic", "fairy"])
+                break
+            case "flying":
+                weaknesses = weaknesses.concat(["rock", "electric", "ice"])
+                break
+            case "poison":
+                weaknesses = weaknesses.concat(["ground", "psychic"])
+                break
+            case "ground":
+                weaknesses = weaknesses.concat(["water", "grass", "ice"])
+                break
+            case "rock":
+                weaknesses = weaknesses.concat(["fighting", "ground", "steel", "water", "grass"])
+            case "bug":
+                weaknesses = weaknesses.concat(["flying", "rock", "fire"])
+                break
+            case "ghost":
+                weaknesses = weaknesses.concat(["ghost", "dark"])
+                break
+            case "steel":
+                weaknesses = weaknesses.concat(["fighting", "ground", "fire"])
+                break
+            case "fire":
+                weaknesses = weaknesses.concat(["ground", "rock", "water"])
+                break
+            case "water":
+                weaknesses = weaknesses.concat(["grass", "electric"])
+                break
+            case "grass":
+                weaknesses = weaknesses.concat(["flying", "poison", "bug", "fire", "ice"])
+                break
+            case "electric":
+                weaknesses = weaknesses.concat(["ground"])
+                break
+            case "psychic":
+                weaknesses = weaknesses.concat(["bug", "ghost", "dark"])
+                break
+            case "ice":
+                weaknesses = weaknesses.concat(["fighting", "rock", "steel", "fire"])
+                break
+            case "dragon":
+                weaknesses = weaknesses.concat(["ice", "dragon", "fairy"])
+                break
+            case "fairy":
+                weaknesses = weaknesses.concat(["poison", "steel"])
+                break
+            case "dark":
+                weaknesses = weaknesses.concat(["fighting", "bug", "fairy"])
+                break
+        }
+    }
+    for (let ctr2 = 0; ctr2 < weaknesses.length; ctr2++) // converts the array to a string
+    {
+        weakString += capitalizeFirstLetter(weaknesses[ctr2]) + " "
+    }
+    return weakString
+}
