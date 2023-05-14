7d97525c6390329722b15f9b4d15535b64d1b34c
Adrian Albino <21102558@usc.edu.ph>
2023-05-14 20:52:23 +0800

    [Feature] Added search function and css changes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const pokemonList = document.getElementById("pokemon-list")
 const loadButton = document.getElementById("load-more")
 const searchInput = document.getElementById("search")
 let count = 0
-let urlNext
+const urlSearch = 'https://pokeapi.co/api/v2/pokemon'
 
 
 /* Fetch names and store in array */
@@ -23,9 +23,7 @@ async function getPokemonData(api)
     const response = await fetch(api)
     const data = await response.json()
     url = data.next
-    console.log(url)
     pokemons = data.results.map(element => element.name)
-    console.log(pokemons)
 }
 
 /* Make an object to store its important details */
@@ -45,7 +43,8 @@ async function getPokemonDetails(api)
     const details = {
         "name" : name,
         "types" : types,
-        "height" : height
+        "height" : height,
+        "id" : data.id
     }
     return details
 }
@@ -53,17 +52,14 @@ async function getPokemonDetails(api)
 async function displayPokemon()
 {
     await getPokemonData(url)
-    for (let x = 0; x < 10; x++) // limit to 10 until
+    for (let x = 0; x < 10; x++) // limit to 10 
     {
-        console.log("hi")
         const details = await getPokemonDetails(`https://pokeapi.co/api/v2/pokemon/${count + 1}/`)
         count++
-        console.log(details)
-        let ID = count
         pokemonList.innerHTML += `
         <div class="pokemon-cards pokemon-card">
             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${count}.png" <br> 
-            <p class="id">ID: ${ID}</p>
+            <p class="id">ID: ${details.id}</p>
             <p class="name"> Name: ${details.name}</p>
             <p class="type"> Types: ${details.types} </p>
         </div>`
@@ -77,7 +73,9 @@ loadButton.addEventListener("click", function()
     displayPokemon()
 })
 
+
 displayPokemon()
+startPokemons = pokemons
 
 
 
@@ -88,10 +86,83 @@ function capitalizeFirstLetter(str) {
 }
 
 
-
-
 /* Adding search functionality */
-searchInput.addEventListener("input", function()
-{
+searchInput.addEventListener("input", async function()
+{   
+    console.log(searchInput.value)
+    const searchValue = searchInput.value.trim()
+    pokemonList.innerHTML = ""
+    let matchesFound = false // determine if match is found
+    if (!isNaN(searchValue)) // checks if value is a number
+    {  
+        // console.log(searchValue)
+        const pokemonId = parseInt(searchValue)
+        if (pokemonId >= 1 && pokemonId <= 1010) // 1010 amount of pokemons in API
+        {
+            const details = await getPokemonDetails(`${urlSearch}/${pokemonId}`)
+            pokemonList.innerHTML += `
+            <div class="pokemon-cards pokemon-card">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png" <br> 
+                <p class="id">ID: ${pokemonId}</p>
+                <p class="name"> Name: ${details.name}</p>
+                <p class="type"> Types: ${details.types} </p>
+            </div>`
+            matchesFound = true
+        }
+    } else // if searchvalue is a name
+    {
+        let searchName = searchValue.toLowerCase()
+        let idStore
+        url = 'https://pokeapi.co/api/v2/pokemon'
+        pokemons = []
+        for (let idCheck = 0; idCheck <= 1010 && matchesFound == false; idCheck += 20) // nested loop to go through entire api
+        {
+            await getPokemonData(url)
+            for (x = 0; x < 20; x++)
+            {
+                if (pokemons[x].toLowerCase().includes(searchName))
+                {
+                    if (idCheck >= 20)
+                    {
+                        idStore = idCheck + x
+                    } 
+                    else {
+                        idStore = x
+                    }
+                    const details = await getPokemonDetails(`${urlSearch}/${idStore+1}`)
+                    pokemonList.innerHTML += `
+                    <div class="pokemon-cards pokemon-card">
+                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${details.id}.png" <br> 
+                        <p class="id">ID: ${details.id}</p>
+                        <p class="name"> Name: ${details.name}</p>
+                        <p class="type"> Types: ${details.types} </p>
+                    </div>`
+                    matchesFound = true
+                }
+            }
+        }
+    } if (!matchesFound && searchInput.value != "")
+    {
+        console.log(searchInput.value)
+        pokemonList.innerHTML = `<p class="error">No results found.</p>`
+    }
+    if(searchInput.value != "") // hides or shows load button when searching
+    {
+        loadButton.style.display = "none"
+    } else 
+    {
+        loadButton.style.display = "block"
+        for (let x = 0; x < 10; x++) // renders the first 10 pokemon when search bar is cleared
+        {
+            const data = await getPokemonDetails(`https://pokeapi.co/api/v2/pokemon/${x + 1}/`)
+            pokemonList.innerHTML += `
+            <div class="pokemon-cards pokemon-card">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${x + 1}.png" <br> 
+                <p class="id">ID: ${data.id}</p>
+                <p class="name"> Name: ${data.name}</p>
+                <p class="type"> Types: ${data.types} </p>
+            </div>`
+        }
+    }
 
 })
